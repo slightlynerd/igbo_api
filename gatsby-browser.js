fd56b747e5a88145cc9166fe9eba5f6efc5abcf8
Ebube Chuba <ebube97@me.com>
2020-10-20 20:32:43 -0700

    delete everything

--- gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-/* Allows the app to apply the styles */
-import './src/styles/tailwind.css';
--- src/config.js
+++
@@ -1,3 +0,0 @@
-const API_ROOT = process.env.NODE_ENV === 'production' ? 'http://igboapi.com' : 'https://cors-anywhere.herokuapp.com/http://www.igboapi.com';
-
-export default `${API_ROOT}/api/v1/search/words`;
